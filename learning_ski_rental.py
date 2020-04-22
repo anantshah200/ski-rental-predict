6767dd69cc3f07f1d5df1a153f27e698102022fa
anantshah200 <anantshah200@gmail.com>
2020-04-22 17:38:15 +0530

    Variation of noise standard deviation

--- learning_ski_rental.py
+++ learning_ski_rental.py
@@ -33,39 +33,46 @@ def get_adv_loss(b_pred,x_pred,b_t,x_t) :
 
 T = 10000
 num_experts = 1000
-B_max = 1
-B_min = 0
+B_max = 4
+B_min = 1
 eps = np.sqrt(np.log(num_experts)/T)
+mu = 0.0
+sigmas = [0.5,1,1.5,2]
 
-b_true = np.random.uniform(low=B_min,high=B_max,size=T) # the true buy values at each instant
-x_adv = np.random.uniform(low=0,high=2*B_max,size=T)
+sigma_regret = []
 
-w_t = np.ones(num_experts) # The initial weights assigned to each expert
-
-true_exp = np.random.randint(low=0,high=num_experts,size=1)
-
-loss = 0.0
-regret = []
-cumul_m = np.zeros(num_experts) # Keep a track on the mistakes made by each expert
-
-for t in range(T) :
-	b_pred = np.random.uniform(low=B_min,high=B_max,size=num_experts)
-	#eps = np.random.normal(mu,sigma,num_experts)
-	#b_pred = b_true[t] + eps
-	x_pred = np.random.uniform(low=0,high=2*B_max,size=num_experts)
-	#b_pred[true_exp] = b_true[t]
-	#x_pred[true_exp] = x_adv[t]
-	p_t = w_t / np.sum(w_t)
-	# Calculate the cost vector <m>
-	m = get_adv_loss(b_pred,x_pred,b_true[t],x_adv[t])
-	w_t = w_t * np.exp(-eps*m)
-	loss += np.dot(p_t,m)
-	cumul_m = cumul_m + m
-	min_loss = np.amin(cumul_m)
-	# We have a true expert so the loss we are comparing with is basically 0
-	regret.append(loss-min_loss)
-
-plt.plot(range(1,T+1),regret)
+for sigma in sigmas :
+	b_true = np.random.uniform(low=B_min,high=B_max,size=T)
+	x_adv = np.random.uniform(low=B_min,high=2*B_max,size=T)
+	loss = 0.0
+	true_exp = np.random.randint(low=0,high=num_experts,size=1)
+	w_t = np.ones(num_experts)
+	regret = []
+	cumul_m = np.zeros(num_experts) # Keep a track on the mistakes by each expert
+	for t in range(T) :
+		b_pred = np.random.uniform(low=B_min,high=B_max,size=num_experts)
+		eps_b = np.random.normal(mu,sigma,num_experts)
+		b_pred = b_pred + eps_b
+		eps_x = np.random.normal(mu,sigma,num_experts)
+		#b_pred = b_true[t] + eps
+		x_pred = np.random.uniform(low=0,high=2*B_max,size=num_experts)
+		x_pred = x_pred + eps_x
+		#b_pred[true_exp] = b_true[t]
+		#x_pred[true_exp] = x_adv[t]
+		p_t = w_t / np.sum(w_t)
+		# Calculate the cost vector <m>
+		m = get_adv_loss(b_pred,x_pred,b_true[t],x_adv[t])
+		w_t = w_t * np.exp(-eps*m)
+		loss += np.dot(p_t,m)
+		cumul_m = cumul_m + m
+		min_loss = np.amin(cumul_m)
+		# We have a true expert so the loss we are comparing with is basically 0
+		regret.append(loss-min_loss)
+	sigma_regret.append(regret)
+	
+for i in range(len(sigmas)) :
+	plt.plot(range(1,T+1),sigma_regret[i])
+plt.legend([r'$\sigma=0.5$',r'$\sigma=1.0$',r'$\sigma=1.5$',r'$\sigma=2.0$'])
 plt.xlabel('Time')
 plt.ylabel('Regret')
 plt.grid('True')
